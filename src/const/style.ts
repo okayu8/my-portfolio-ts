7e0be1516073c58ef8a4722cd207508b4383fe6f
片岡祐太郎 <hitugaya12@gmail.com>
2019-08-14 13:54:35 +0900

    create style const

--- src/const/style.ts
+++ src/const/style.ts
@@ -1,4 +1,18 @@
-export namespace Style {
-  export const BUTTON_PRIMARY = "P";
-  export const BUTTON_WARN = "W";
+export namespace STYLE {
+  export namespace BUTTON {
+    export const PRIMARY = "P";
+    export const WARN = "W";
+  }
+  export namespace SIZE {
+    export const SMALL = 4;
+    export const NOMAL = 8;
+    export const LARGE = 16;
+    export const LARGEX = 24;
+    export const LARGEXX = 32;
+    export const LARGEXXX = 40;
+  }
+  export namespace FONTSIZE {
+    export const SMALL = 14;
+    export const NOMAL = 16;
+  }
 }
