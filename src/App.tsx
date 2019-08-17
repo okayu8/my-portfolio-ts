6a95ca838a23628c01f024ff0e7898210fbefe98
片岡祐太郎 <hitugaya12@gmail.com>
2019-08-18 03:29:53 +0900

    title apear timming

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,14 @@ class App extends React.Component {
       <div className="App">
         <div className="wrap">
           <div className="App-content">
-            <span className="App-back-first">Kataoka</span>
-            <span className="App-back-second">Yutaro</span>
-            <span className="App-title-first">Kataoka</span>
-            <span className="App-title-second">Yutaro</span>
+            <div className="App-back">
+              <span className="App-back-first">Kataoka</span>
+              <span className="App-back-second">Yutaro</span>
+            </div>
+            <div className="App-title">
+              <span className="App-title-first">Kataoka</span>
+              <span className="App-title-second">Yutaro</span>
+            </div>
             <div className="box">
               <div className="circle layer-0" />
               <div className="circle layer-1" />
