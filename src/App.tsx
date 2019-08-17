2aee85e298c76d44f3cfc08df3bc2131d71c517b
片岡祐太郎 <hitugaya12@gmail.com>
2019-08-18 02:07:46 +0900

    add loading and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import * as React from "react";
 import "./App.css";
-import TsTest from "./components/TsTest";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <div className="App-content">
-          <h1 className="App-title">Welcome to React</h1>
-          <TsTest text="test" />
-          <div className="wrap">
-            {/* 読み込みが完了したら表示する要素 */}
-            <p>読み込み完了</p>
+        <div className="wrap">
+          <div className="App-content">
+            <h1 className="App-title-first">Kataoka</h1>
+            <h1 className="App-title-second">Yutaro</h1>
+            <div className="box">
+              <div className="circle layer-0" />
+              <div className="circle layer-1" />
+              <div className="circle layer-2" />
+            </div>
           </div>
-          <div className="box">
-            <div className="circle layer-0" />
-            <div className="circle layer-1" />
-            <div className="circle layer-2" />
-          </div>
+          <div className="App-footer" />
+          <div className="App-side" />
         </div>
-        <div className="App-footer" />
-        <div className="App-side" />
       </div>
     );
   }
