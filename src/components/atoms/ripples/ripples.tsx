04c2c208dbec9774c549e3fce91a86f765a14400
片岡祐太郎 <hitugaya12@gmail.com>
2019-08-22 23:32:29 +0900

    add Ripples component

---
+++ src/components/atoms/ripples/ripples.tsx
@@ -0,0 +1,104 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const Ripples = () => {
+  return (
+    <StyledBox>
+      <StyledCircle1 />
+      <StyledCircle2 />
+      <StyledCircle3 />
+    </StyledBox>
+  );
+};
+
+const StyledBox = styled.div`
+  width: 8vw;
+  height: 8vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const StyledCircle1 = styled.div`
+  width: 80%;
+  height: 80%;
+  border: solid thin #aaacbe60;
+  border-radius: 100%;
+  position: absolute;
+  animation: wave-0 5s infinite;
+
+  @keyframes wave-0 {
+    0% {
+      width: 0%;
+      height: 0%;
+      box-shadow: 0 0 100px inset #8585853f;
+    }
+    50% {
+      width: 100%;
+      height: 100%;
+      opacity: 0;
+    }
+    100% {
+      width: 0%;
+      height: 0%;
+      opacity: 0;
+    }
+  }
+`;
+
+const StyledCircle2 = styled.div`
+  width: 80%;
+  height: 80%;
+  border: solid thin #aaacbe60;
+  border-radius: 100%;
+  position: absolute;
+  animation: wave-1 5s infinite;
+
+  @keyframes wave-1 {
+    0% {
+      width: 0%;
+      height: 0%;
+      box-shadow: 0 0 100px inset #8585853f;
+    }
+    50% {
+      width: 75%;
+      height: 75%;
+      opacity: 0;
+    }
+    100% {
+      width: 0%;
+      height: 0%;
+      opacity: 0;
+    }
+  }
+`;
+
+const StyledCircle3 = styled.div`
+  width: 80%;
+  height: 80%;
+  border: solid thin #aaacbe60;
+  border-radius: 100%;
+  position: absolute;
+  animation: wave-2 5s infinite;
+
+  @keyframes wave-2 {
+    0% {
+      width: 0%;
+      height: 0%;
+      box-shadow: 0 0 100px inset #8585853f;
+    }
+    50% {
+      width: 50%;
+      height: 50%;
+      opacity: 0;
+    }
+    100% {
+      width: 0%;
+      height: 0%;
+      opacity: 0;
+    }
+  }
+`;
+
+export default Ripples;
