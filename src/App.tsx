920c651b5879406d1bf488d2c6b02b7caf370156
片岡祐太郎 <hitugaya12@gmail.com>
2019-08-27 23:15:00 +0900

    allowJs true

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,6 @@ class App extends React.Component {
               <span className="App-title-first">Kataoka</span>
               <span className="App-title-second">Yutaro</span>
             </div>
-            <div className="box">
-              <div className="circle layer-0" />
-              <div className="circle layer-1" />
-              <div className="circle layer-2" />
-            </div>
           </div>
           <div className="App-footer" />
           <div className="App-side" />
