23450f58ec1ea82a2c83c719ae92e75a18aca577
片岡祐太郎 <hitugaya12@gmail.com>
2019-09-03 00:02:50 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import * as React from "react";
 import "./App.css";
 import Top from "./components/pages/top";
+import {
+  Route,
+  // Link,
+  HashRouter as Router
+} from "react-router-dom";
+
+export const test: any = "test";
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <div className="wrap">
-          <div className="App-content">
-            <Top />
+      <Router>
+        <div className="App">
+          <div className="wrap">
+            <div className="App-content">
+              <Route exact={true} path="/" component={Top} />
+            </div>
+            <div className="App-footer" />
+            <div className="App-side" />
           </div>
-          <div className="App-footer" />
-          <div className="App-side" />
         </div>
-      </div>
+      </Router>
     );
   }
 }
