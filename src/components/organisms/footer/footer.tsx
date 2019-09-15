0029abb05bf26e49b39ad9091d4275010690b39d
片岡祐太郎 <hitugaya12@gmail.com>
2019-09-15 13:44:41 +0900

    styling footer

--- src/components/organisms/footer/footer.tsx
+++ src/components/organisms/footer/footer.tsx
@@ -5,8 +5,8 @@ import Button from "../../atoms/button";
 const Footer = () => {
   return (
     <StyledFooter>
-      <BUTTON to={""} name={"GitHub"} />
-      <BUTTON to={""} name={"Email"} />
+      <Button to={""} name={"GitHub"} />
+      <Button to={""} name={"Email"} />
     </StyledFooter>
   );
 };
@@ -14,16 +14,12 @@ const Footer = () => {
 const StyledFooter = styled.div`
   width: 120%;
   height: 300px;
-  padding-left: 30%;
+  padding-left: 70%;
   padding-top: 32px;
   background-color: #474747;
   transform-origin: left bottom;
   transform: rotate(-5deg);
   display: flex;
-`;
-
-const BUTTON = styled(Button)`
-  margin-left: 8px;
 `;
 
 export default Footer;
