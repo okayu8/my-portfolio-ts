5e7c81529042e92b3e2e94a54b584912eee9721b
片岡祐太郎 <hitugaya12@gmail.com>
2019-12-04 00:02:12 +0900

    styling profile delay

--- src/components/pages/profile/profile.tsx
+++ src/components/pages/profile/profile.tsx
@@ -16,6 +16,16 @@ const Profile = () => {
 
 const StyledProfile = styled.div`
   text-align: left;
+  animation: fadeIn-1 2s ease 0s 1 normal;
+  -webkit-animation: fadeIn-1 2s ease 0s 1 normal;
+  @keyframes fadeIn-1 {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+  }
 `;
 
 const StyledName = styled.p`
